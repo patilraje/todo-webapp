bb3980108eba56ad3b60df91fb2bdd1fb49a7315
hawa <rp@catenact.io>
2026-03-15 23:38:16 -0700

    add task completion with checkbox

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
 let tasks = JSON.parse(localStorage.getItem("tasks")) || []
 
 function renderTasks() {
+
     let list = document.getElementById("taskList")
     list.innerHTML = ""
 
     tasks.forEach((task, index) => {
-        let li = document.createElement("li")
-        li.textContent = task
 
-        li.onclick = function() {
-            tasks.splice(index, 1)
+        let li = document.createElement("li")
+
+        let checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.checked = task.completed
+
+        checkbox.onchange = function(){
+            tasks[index].completed = checkbox.checked
             saveTasks()
             renderTasks()
         }
+
+        let span = document.createElement("span")
+        span.textContent = task.text
+
+        if(task.completed){
+            span.classList.add("completed")
+        }
+
+        span.onclick = function(){
+            tasks.splice(index,1)
+            saveTasks()
+            renderTasks()
+        }
+
+        li.appendChild(checkbox)
+        li.appendChild(span)
 
         list.appendChild(li)
     })
 }
 
-function addTask() {
+function addTask(){
 
     let input = document.getElementById("taskInput")
-    let task = input.value
 
-    if(task === "") return
+    if(input.value === "") return
 
-    tasks.push(task)
+    tasks.push({
+        text: input.value,
+        completed:false
+    })
 
     saveTasks()
     renderTasks()
 
-    input.value = ""
+    input.value=""
 }
 
 function saveTasks(){
