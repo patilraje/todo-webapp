e737a10fe5dfee883b634eb649d444064ab3dee8
hawa <rutujapatilonline@gmail.com>
2026-03-31 19:52:37 -0700

    add task stats (total, completed, remaining)

--- script.js
+++ script.js
@@ -104,7 +104,8 @@ if(task.dueDate && !task.completed){
     let remaining = total - completed
 
     document.getElementById("taskCount").textContent =
-        `${remaining} remaining | ${total} total | ${completed} completed`
+    `${remaining} remaining | ${total} total | ${completed} completed`
+}
 
 
 // ✅ Add task
